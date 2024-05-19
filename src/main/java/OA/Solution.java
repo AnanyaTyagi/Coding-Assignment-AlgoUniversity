12519d6d2260a9b268fd4c861aec14a62b91db41
a13401626 <ananya.tyagi@africa.airtel.com>
2024-05-20 02:21:38 +0530

    abc

---
+++ src/main/java/OA/Solution.java
@@ -0,0 +1,61 @@
+package OA;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String args[]){
+        Scanner sc  = new Scanner(System.in);
+        // you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+import java.util.*;
+
+        class Solution {
+            public String solution(String S, int K) {
+                // Implement your solution here
+
+                char[] arr = S.toCharArray();
+                HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+                int countOf5 = 0;
+                for(int i = 0; i <arr.length;i++){
+                    if(map.containsKey(Integer.valueOf(arr[i])))
+                        map.put(Integer.valueOf(arr[i]), map.get(Integer.valueOf(arr[i]))+1);
+                    else
+                        map.put(Integer.valueOf(arr[i]), 1);
+
+
+                }
+//  List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+//   Collections.sort(list,new Comparator< Map.Entry<String, Integer>>(){
+//       return o1.getValue().compareTo(o2.getValue());
+//   });
+
+                if(!(map.get(5)==null)){
+                    int difference= S.length()- map.get(5);
+                    if(difference<K)
+                        return  "IMPOSSIBLE";
+                }
+
+                char solution[] = new char[arr.length];
+                for(int i = 0; i <arr.length;i++){
+                    if(K>0){
+                        if(Integer.valueOf(arr[i])<5)
+                            solution[i]=5;
+                    } else{
+                        solution[i] = arr[i];
+                        break;
+                    }
+
+                    K--;
+                }
+
+                return new String(solution);
+            }
+        }
+
+
+    }
+}
